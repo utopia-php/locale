04ba7b14b675f5495d770395fdf0a1632658dad9
eldadfux <eldad.fux@gmail.com>
2019-04-28 22:33:16 +0300

    Added source code

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
-# locale
+# Utopia Framework Locale
+
+Utopia framework locale library is simple and lite library for managing application translations and localization. This library is aiming to be as simple and easy to learn and use.
+
+Although this library is part of the [Utopia Framework](https://github.com/utopia-php/framework) project it is dependency free and can be used as standalone with any other PHP project or framework.
+
+## Getting Started
+
+```php
+<?php
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+
+use Utopia\Locale\Locale;
+
+// Init translations
+Locale::setLanguage('en-US', ['hello' => 'Hello','world' => 'World']); // Set English
+Locale::setLanguage('he-IL', ['hello' => 'שלום',]); // Set Hebrew, Any missing items will fallback to english
+
+// Get translation
+
+echo Locale::getText('hello'); // prints "שלום"
+echo Locale::getText('world'); // prints "World"
+```
+
+## System Requirements
+
+Utopia Framework requires PHP 7.1 or later. We recommend using the latest PHP version whenever possible.
+
+## Authors
+
+**Eldad Fux**
+
++ [https://twitter.com/eldadfux](https://twitter.com/eldadfux)
++ [https://github.com/eldadfux](https://github.com/eldadfux)
+
+## Copyright and license
+
+The MIT License (MIT) [http://www.opensource.org/licenses/mit-license.php](http://www.opensource.org/licenses/mit-license.php)
---
+++ src/Locale/Locale.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Utopia\Locale;
+
+use Exception;
+
+class Locale
+{
+    /**
+     * @var array
+     */
+    static protected $language = [];
+
+    /**
+     * Throw Exceptions?
+     *
+     * @var bool
+     */
+    static public $exceptions = true;
+
+    /**
+     * Default Locale
+     *
+     * @var null
+     */
+    static public $default = null;
+
+    /**
+     * Set New Locale
+     *
+     * @param string $name
+     * @param array $language
+     * @return array
+     */
+    static public function setLanguage($name, array $language) //TODO add support for lazy load to memory
+    {
+        if(empty(self::$default)) {
+            self::$default = $name;
+        }
+
+        return self::$language[$name] = $language;
+    }
+
+    /**
+     * Change Default Locale
+     *
+     * @param $name
+     * @throws Exception
+     */
+    static public function setDefault($name)
+    {
+        if(!array_key_exists($name, self::$language)) {
+            throw new Exception('Locale not found');
+        }
+
+        self::$default = $name;
+    }
+
+    /**
+     * Get Text by Locale
+     *
+     * @param $key
+     * @param null $default
+     * @return string
+     * @throws Exception
+     */
+    static public function getText($key, $default = null) {
+        $default = (is_null($default)) ? '{{' . $key . '}}' : $default;
+
+        if(!array_key_exists($key, self::$language[self::$default])) {
+            if(self::$exceptions) {
+                throw new Exception('Key named "' . $key . '" not found');
+            }
+
+            return $default;
+        }
+
+        return self::$language[self::$default][$key];
+    }
+}
